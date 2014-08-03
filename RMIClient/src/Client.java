c122aea57b2d215765f2a441fb2915fc289a5b51
jinlongfeng <sunnybird@users.noreply.github.com>
2014-08-03 22:06:53 +0800

    Update Client.java

--- RMIClient/src/Client.java
+++ RMIClient/src/Client.java
@@ -1,5 +1,5 @@
 /**
- * 创建服务实体并绑定
+ * 客户端调用远程服务
  */
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -10,7 +10,7 @@ public class Client {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		// TODO Auto-generated method stub
-		Calculator calc=(Calculator)Naming.lookup("rmi://127.0.0.1:6600/CalculatorService"); 
+	Calculator calc=(Calculator)Naming.lookup("rmi://127.0.0.1:6600/CalculatorService"); 
 	    System.out.println(calc.add(10, 10));
 	    System.out.println(calc.sub(10, 10));
 	    System.out.println(calc.div(10, 10));
