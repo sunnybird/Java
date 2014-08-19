54bdc254b38bad712459a0e4fc02ed31f6dbcc2e
jinlong <jinlongfeng123@gmail.com>
2014-08-20 00:35:03 +0800

    delete some code

--- RMIClient/src/Client.java
+++ RMIClient/src/Client.java
@@ -9,7 +9,6 @@ import java.rmi.RemoteException;
 public class Client {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
-		// TODO Auto-generated method stub
 	Calculator calc=(Calculator)Naming.lookup("rmi://127.0.0.1:6600/CalculatorService"); 
 	    System.out.println(calc.add(10, 10));
 	    System.out.println(calc.sub(10, 10));
